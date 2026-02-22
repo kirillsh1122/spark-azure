96d36318fca27c5ecb6155103d38767f7716b0f8
Kiryl Shapialevich <kirillshepelevich@gmail.com>
2026-02-22 18:03:47 +0100

    refactored Schemamanager

--- src/main/java/com/sparkpractice/dse/utils/SchemaManager.java
+++ src/main/java/com/sparkpractice/dse/utils/SchemaManager.java
@@ -1,16 +1,15 @@
 package com.sparkpractice.dse.utils;
 
+
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.types.DataType;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+public class SchemaManager {
 
-public class Utils {
-
-    public static StructType getSchema(String pathToSchema) throws IOException {
+    public static StructType getSchemaFromFile(String pathToSchema) throws IOException {
         String schemaString = new String(Files.readAllBytes(Paths.get(pathToSchema)));
         return (StructType) DataType.fromJson(schemaString);
     }
