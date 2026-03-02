598053c9da6166b8ae6b1b148ef7080f5feadc75
Kiryl Shapialevich <kirillshepelevich@gmail.com>
2026-03-02 22:45:01 +0100

    Fixed Schemamanager to pull schema file from JAR resource

--- src/main/java/com/sparkpractice/dse/utils/SchemaManager.java
+++ src/main/java/com/sparkpractice/dse/utils/SchemaManager.java
@@ -4,13 +4,28 @@ package com.sparkpractice.dse.utils;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.types.DataType;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class SchemaManager {
 
-    public static StructType getSchemaFromFile(String pathToSchema) throws IOException {
-        String schemaString = new String(Files.readAllBytes(Paths.get(pathToSchema)));
-        return (StructType) DataType.fromJson(schemaString);
+    public static StructType getSchemaFromFile(String resourcePath) {
+
+        try (
+                InputStream is = Thread.currentThread()
+                        .getContextClassLoader()
+                        .getResourceAsStream(resourcePath)) {
+
+            if (is == null) {
+                throw new IllegalArgumentException("Schema not found in resources: " + resourcePath);
+            }
+
+            String schemaString = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+
+            return (StructType) DataType.fromJson(schemaString);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load schema: " + resourcePath, e);
+        }
     }
 }
